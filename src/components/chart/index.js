5344cc5736a4650a35a92abc75ca76c7411d8a45
Azar-kh <khodadadi.azar@yahoo.com>
2022-11-29 14:57:05 -0500

    chart2

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 import { Line } from 'react-chartjs-2'
-import { Chart } from 'react-chartjs-2'
+import { Chart as ChartJS } from 'chart.js/auto'
 
 const Chart = ({ data }) => {
   const chartData = {
